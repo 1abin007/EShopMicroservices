ac276028d033015e95a025a8feb86cd0a87c7acf
abinv <abinvnovigo@gmail.com>
2024-10-30 10:48:56 +0530

    catalog implemention

---
+++ src/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Product
+{
+	public Product()
+	{
+	}
+
+	public Guid Id { get; set; }
+
+	public string Name { get; set; } = default!;
+
+	public string Category { get; set; }
+
+	public string Description { get; set; }
+}
